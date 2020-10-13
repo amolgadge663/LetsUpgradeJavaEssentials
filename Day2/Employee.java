46704da7acf4396fe586906ca48da41fab4afbdd
Amol <amolgadge663@gmail.com>
2020-10-13 11:50:19 +0530

    Day2 Assignment Answers Submission

---
+++ Day2/Employee.java
@@ -0,0 +1,14 @@
+
+public class Employee {
+
+    String name = "Amol Gadage";
+    int age = 23;
+    String city = "Sangamner";
+
+    public void display(){
+        System.out.println("The name is " + name); 
+        System.out.println("The age is " + age); 
+        System.out.println("The city is " + city); 
+    }
+
+}
---
+++ Day2/MainFIle.java
@@ -0,0 +1,12 @@
+
+class MainClass{
+
+    public static void main(String[] args) {
+        
+        Employee e1 = new Employee();
+        Employee e2 = new Employee();
+
+        e1.display();
+        e2.display();
+    }
+}
